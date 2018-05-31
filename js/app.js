f077210388ce62be3e40f572778d027608c258a1
Stefanos Gkouveris <stefanos.gkouveris@gmail.com>
2018-05-31 17:52:22 +0100

    Shuffle function
    - Add shuffle function to load with a browser refresh

--- js/app.js
+++ js/app.js
@@ -71,6 +71,34 @@
  }
 
 
+  //Shuffle function
+  const deckShuffle = document.querySelector(".deck");
+
+  function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      while (currentIndex !== 0) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+  }
+
+  function startGame(){
+     var shuffledCards = shuffle(cards);
+     for (var i= 0; i < shuffledCards.length; i++){
+        [].forEach.call(shuffledCards, function(item){
+           deckShuffle.appendChild(item);
+        });
+     }
+  }
+
+  //start the game with browser refresh
+  window.onload = startGame();
 // The end of shuffle function
 
 /*
