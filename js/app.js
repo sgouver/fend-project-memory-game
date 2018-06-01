e5b7f7f18aac49d97fa9f13187259dbf35a9650f
Stefanos Gkouveris <stefanos.gkouveris@gmail.com>
2018-06-01 20:34:49 +0100

    Add measuring features
    - Added Star rating system
    - Added Stopwatch features
    - Added Moves counter

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
  let card = document.querySelectorAll(".card");
  let cards = [...card];
 
+ //Create a list that holds all of the rating stars
+ let star = document.querySelectorAll(".star");
+ let stars = [...star];
+
  // Store open cards
  let openedCards = [];
 
@@ -10,6 +14,7 @@
   for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", cardOpen);
   cards[i].addEventListener("click", display1stCard);
+  cards[i].addEventListener("click", moveCounter);
  }
 
 //Allow cards to be clicked
@@ -54,6 +59,14 @@
      },1200);
  }
 
+ //Move counter functionality
+ let moves = 0;
+ function moveCounter(){
+   moves += 1
+   document.querySelector(".moves").innerHTML = moves ;
+   rating()
+ }
+
  //Disable clicking cards when two cards are open
  function disable(){
    let i;
@@ -70,6 +83,61 @@
    }
  }
 
+ //Add refresh button functionality
+ const restart = document.querySelector(".restart");
+
+ restart.addEventListener("click",
+ function () {
+   let i;
+   for (i=0; i<cards.length; i++){
+     cards[i].classList.remove("show", "open", "match");
+   }
+   for (i=1; i<stars.length; i++){
+     stars[i].classList.remove("far")
+     stars[i].classList.add("fas")
+   }
+   startGame()
+   setInterval()
+ }
+);
+
+  //Add Star rating system
+  function rating(){
+    if (moves > 15 && moves < 29) {
+      stars[2].classList.remove("fas")
+      stars[2].classList.add("far")
+    }
+    else if (moves > 30) {
+      stars[1].classList.remove("fas")
+      stars[1].classList.add("far")
+    }
+  }
+
+
+  //Set a timer
+let time = 0 ;
+function timer(){
+          setTimeout(function(){
+              time++;
+              let min = Math.floor(time/100/60);
+              let sec = Math.floor(time/100);
+              let mSec = time % 100;
+
+              if(min < 10) {
+                  min = "0" + min;
+              }
+              if(sec >= 60) {
+                  sec = sec % 60;
+              }
+              if(sec < 10) {
+                  sec = "0" + sec;
+              }
+
+              document.querySelector("#timer").innerHTML = min + ":" + sec;
+              timer();
+          }, 10);
+    }
+    timer();
 
   //Shuffle function
   const deckShuffle = document.querySelector(".deck");
@@ -89,6 +157,10 @@
   }
 
   function startGame(){
+    moves = 0;
+    time = 0;
+    document.querySelector("#timer").innerHTML = '00:00:00';
+    document.querySelector(".moves").innerHTML = moves
      var shuffledCards = shuffle(cards);
      for (var i= 0; i < shuffledCards.length; i++){
         [].forEach.call(shuffledCards, function(item){
@@ -102,12 +174,6 @@
 // The end of shuffle function
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
